68e681df0fd2054ac2c1a6881b69c2338e13c7e7
Cristian Mansilla <cristianezequielmansilla@hotmail.com>
2022-10-14 10:13:48 -0300

    :bug: Fix: Add data type as a comment

--- script.js
+++ script.js
@@ -6,12 +6,25 @@ _Colocar en un comentario el tipo de dato de esa variable.
 _Al menos 1 de las variables debe ser un objeto con un mínimo de 5 propiedades y 1 debe ser array.
 */
 
-const nombre = "Cristian";
-const apellido = "Mansilla";
+//data type: string
+var nombre = "Cristian";
+
+//data type: string
+var apellido = "Mansilla";
+
+//data type: string
 const equipoDeFutbol = "Boca Juniors";
+
+//data type: string
 var colorFavorito = "Azul";
+
+//data type: boolean
 var tieneHijos = false;
+
+//data type: array
 var numerosQuini6 = [00, 10, 20, 25, 30, 40, 45];
+
+//data type: object
 var queAprendiEnIniciatec = {
     tecnologias: {
         HTML: {
@@ -39,6 +52,8 @@ var queAprendiEnIniciatec = {
         }
     }
 }
+
+//data type: object
 var vehiculo = {
     moto: {
         marca: "Yamaha",
@@ -46,11 +61,19 @@ var vehiculo = {
         color: "Azul"
     }
 }
+
+//data type: boolean
 var tieneHermanos = true;
+
+//data type: string
 var pc = "laptop";
+
+//data type: number
+var numeroCamiseta = 11;
 
 console.log(`${nombre} ${apellido} tiene como color favorito al ${colorFavorito} y es hincha de: ${equipoDeFutbol}.`);
 console.log(`Se dirige en su ${vehiculo.moto.modelo} a jugar el quini 6 a los números: ${numerosQuini6}`);
 console.log(`Todas las tardes de lunes a viernes se conecta con su ${pc} a las clases de IniciaTec. Lo que aprendió hasta el momento es: ${queAprendiEnIniciatec.tecnologias.HTML.nombre} (HTML): las siguientes etiquetas - ${queAprendiEnIniciatec.tecnologias.HTML.etiquetas} ${queAprendiEnIniciatec.tecnologias.CSS.nombre} (CSS): las siguientes propiedades - ${queAprendiEnIniciatec.tecnologias.CSS.propiedades} ${queAprendiEnIniciatec.tecnologias.JS.nombre} (JS): los siguientes contenidos - ${queAprendiEnIniciatec.tecnologias.JS.contenido}.`);
 console.log(`En una entrevista de trabajo le preguntaron si tenia hijos y él respondio: ${tieneHijos}. Luego le preguntaron si tenia hermanos a lo que contestó: ${tieneHermanos}.`);
+console.log(`El domingo pasado fue a jugar al fútbol, le dieron la camiseta ${numeroCamiseta} porque juega de extremo y fue decisivo para la victoria de su equipo con 1 gol y dos asistencias.`);
 
