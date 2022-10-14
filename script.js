99b6dc136f7f2768e9bb12a64df2244fd3ad5f40
Cristian Mansilla <cristianezequielmansilla@hotmail.com>
2022-10-14 10:23:58 -0300

    .sparkles: Feat: Add other console log

--- script.js
+++ script.js
@@ -74,6 +74,7 @@ var numeroCamiseta = 11;
 console.log(`${nombre} ${apellido} tiene como color favorito al ${colorFavorito} y es hincha de: ${equipoDeFutbol}.`);
 console.log(`Se dirige en su ${vehiculo.moto.modelo} a jugar el quini 6 a los números: ${numerosQuini6}`);
 console.log(`Todas las tardes de lunes a viernes se conecta con su ${pc} a las clases de IniciaTec. Lo que aprendió hasta el momento es: ${queAprendiEnIniciatec.tecnologias.HTML.nombre} (HTML): las siguientes etiquetas - ${queAprendiEnIniciatec.tecnologias.HTML.etiquetas} ${queAprendiEnIniciatec.tecnologias.CSS.nombre} (CSS): las siguientes propiedades - ${queAprendiEnIniciatec.tecnologias.CSS.propiedades} ${queAprendiEnIniciatec.tecnologias.JS.nombre} (JS): los siguientes contenidos - ${queAprendiEnIniciatec.tecnologias.JS.contenido}.`);
+console.log(`En IniciaTec desarrolló: ${queAprendiEnIniciatec.proyectos.IniciaFood.descripción}, de nombre IniciaFood. Junto al equipo Rocket-Dev que se componia de: ${queAprendiEnIniciatec.proyectos.IniciaFood.equipo}. Para lograrlo tuvieron que utilizar la metodología SCRUM: ${queAprendiEnIniciatec.metodologias.SCRUM.definicion}.`);
 console.log(`En una entrevista de trabajo le preguntaron si tenia hijos y él respondio: ${tieneHijos}. Luego le preguntaron si tenia hermanos a lo que contestó: ${tieneHermanos}.`);
 console.log(`El domingo pasado fue a jugar al fútbol, le dieron la camiseta ${numeroCamiseta} porque juega de extremo y fue decisivo para la victoria de su equipo con 1 gol y dos asistencias.`);
 
